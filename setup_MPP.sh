3d50ce1e5d67d0bb4b94b3edc7f91598b3faaf0e
Stefan Kluth <skluth@mpp.mpg.de>
2014-04-04 16:19:33 +0200

    add environment setup

---
+++ setup_MPP.sh
@@ -0,0 +1,6 @@
+export RIVET_ANALYSIS_PATH=$PWD::
+export RIVET_REF_PATH=$PWD::
+export RIVET_INFO_PATH=$PWD::
+export RIVET_PLOT_PATH=$PWD::
+export PYTHONPATH=$HOME/qcd/sherpa/local/lib/python2.7/site-packages
+export PATH=$PATH:$HOME/qcd/sherpa/local/bin
