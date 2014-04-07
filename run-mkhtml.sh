152afd7529628e20e85659cc912cb267955f49ca
andriish <andriish@users.noreply.github.com>
2014-04-07 22:31:46 +0200

    All energies, but not all plots are OK

--- run-mkhtml.sh
+++ run-mkhtml.sh
@@ -9,7 +9,7 @@ export RIVET_PLOT_PATH=$(pwd)/ref::
 rivet-mkhtml Rivet.yoda  ref/$1.yoda -o out
 sed -i 's@LineStyle=solid@PolyMarker=*@g' $(ls -1 out/$1/d14*.dat  out/$1/d13*.dat out/$1/d12*.dat out/$1/d11*.dat  out/$1/d10*.dat out/$1/d09*.dat out/$1/d08*.dat  out/$1/d07*.dat )
 
-sed -i 's@LineStyle=solid@PolyMarker=*@g' $(ls -1 out/$1/d23*.dat  out/$1/d22*.dat out/$1/d21*.dat out/$1/d30*.dat  out/$1/d10*.dat out/$1/d09*.dat out/$1/d08*.dat  out/$1/d07*.dat )
+sed -i 's@LineStyle=solid@PolyMarker=*@g' $(ls -1 out/$1/d23*.dat  out/$1/d22*.dat out/$1/d21*.dat out/$1/d21*.dat  out/$1/d20*.dat out/$1/d19*.dat out/$1/d18*.dat  out/$1/d17*.dat  out/$1/d16*.dat )
 
 
 make-plots --pdfpng out/*.dat   out/$1/*.dat
