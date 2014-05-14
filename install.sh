870bd4faa4db856a71365b56a3c0287eeafa787e
andriish <andriish@users.noreply.github.com>
2014-05-14 17:50:04 +0200

    Remove debug packages and change install script

--- install.sh
+++ install.sh
@@ -15,33 +15,33 @@ wget http://linuxsoft.cern.ch/epel/6/i386/cernlib-utils-2006-35.el6.i686.rpm
 
 
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/AGILe-1.4.1-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/AGILe-debuginfo-1.4.1-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/AGILe-debuginfo-1.4.1-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/AGILe-devel-1.4.1-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/HepMC-2.06.09-3.el6.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/HepMC-debuginfo-2.06.09-3.el6.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/HepMC-debuginfo-2.06.09-3.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/HepMC-devel-2.06.09-3.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Herwig++-2.7.0-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Herwig++-debuginfo-2.7.0-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Herwig++-debuginfo-2.7.0-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Herwig++-devel-2.7.0-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/PHOTOS-3.54-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/PHOTOS-debuginfo-3.54-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/PHOTOS-debuginfo-3.54-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/PHOTOS-devel-3.54-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-8.1.80-1.el6.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-debuginfo-8.1.80-1.el6.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-debuginfo-8.1.80-1.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-devel-8.1.80-1.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-hepmcinterface-8.1.80-1.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/pythia8-hepmcinterface-devel-8.1.80-1.el6.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Rivet-2.1.1-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Rivet-debuginfo-2.1.1-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Rivet-debuginfo-2.1.1-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/Rivet-devel-2.1.1-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/TAUOLA-1.1.4-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/TAUOLA-debuginfo-1.1.4-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/TAUOLA-debuginfo-1.1.4-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/TAUOLA-devel-1.1.4-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/ThePEG-1.9.0-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/ThePEG-debuginfo-1.9.0-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/ThePEG-debuginfo-1.9.0-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/ThePEG-devel-1.9.0-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/YODA-1.0.6-1.x86_64.rpm
-wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/YODA-debuginfo-1.0.6-1.x86_64.rpm
+#wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/YODA-debuginfo-1.0.6-1.x86_64.rpm
 wget https://www.mpp.mpg.de/~andriish/rpms/x86_64/YODA-devel-1.0.6-1.x86_64.rpm
 
 
@@ -104,3 +104,18 @@ cd top
 for a in $(ls -1 ../*rpm); do
 rpm2cpio $a | cpio -i --make-directories 
 done
+
+
+. init-post-install.sh
+
+cp $(which rivet-buildplugin) $(which rivet-buildplugin).old
+cat $(which rivet-buildplugin).old  | sed  's@yoda="/usr@yoda="'$(pwd)/top/usr'@g' | sed  's@hepmc="/usr@hepmc="'$(pwd)/top/usr'@g' | sed  's@rivet="/usr@rivet="'$(pwd)/top/usr'@g' | sed  's@ifastjet="/usr@ifastjet="'$(pwd)/top/usr'@g' | sed  's@lfastjet="@lfastjet="-L'$(pwd)/top/usr'@g'  > $(which rivet-buildplugin).new
+
+
+cp $(which yoda-config)  $(which yoda-config).old
+cat $(which yoda-config).old  |      sed  's@/usr/bin/env\ bash@/bin/bash@g' | sed  's@/usr@'$(pwd)/top/usr'@g' >  $(which yoda-config)
+
+
+
+cp $(which root-config)  $(which root-config).old
+cat $(which root-config).old  | sed  's@dir=/usr@dir='$(pwd)/top/usr'@g' >  $(which root-config)
