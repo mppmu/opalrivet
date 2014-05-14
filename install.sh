9d061486a20390c0ab9a0f06ece09cc03100ab34
andriish <andriish@users.noreply.github.com>
2014-05-14 19:00:44 +0200

    tiny fix

--- install.sh
+++ install.sh
@@ -108,11 +108,12 @@ for a in $(ls -1 ../RPMS/*rpm); do
 rpm2cpio $a | cpio -i --make-directories 
 done
 
+set -x
 cd ..
 . ./init-post-install.sh
 
 cp $(which rivet-buildplugin) $(which rivet-buildplugin).old
-cat $(which rivet-buildplugin).old  | sed  's@yoda="/usr@yoda="'$(pwd)/top/usr'@g' | sed  's@hepmc="/usr@hepmc="'$(pwd)/top/usr'@g' | sed  's@rivet="/usr@rivet="'$(pwd)/top/usr'@g' | sed  's@ifastjet="/usr@ifastjet="'$(pwd)/top/usr'@g' | sed  's@lfastjet="@lfastjet="-L'$(pwd)/top/usr'@g'  > $(which rivet-buildplugin).new
+cat $(which rivet-buildplugin).old  | sed  's@yoda="/usr@yoda="'$(pwd)/top/usr'@g' | sed  's@hepmc="/usr@hepmc="'$(pwd)/top/usr'@g' | sed  's@rivet="/usr@rivet="'$(pwd)/top/usr'@g' | sed  's@ifastjet="/usr@ifastjet="'$(pwd)/top/usr'@g' | sed  's@lfastjet="@lfastjet="-L'$(pwd)/top/usr'@g'  > $(which rivet-buildplugin)
 
 
 cp $(which yoda-config)  $(which yoda-config).old
