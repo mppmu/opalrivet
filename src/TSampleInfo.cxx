d5e6838d8d342349502a1a57786f8f1afb4dd412
andriish <andriish@users.noreply.github.com>
2015-06-20 07:19:29 +0200

    OK

---
+++ src/TSampleInfo.cxx
@@ -0,0 +1,52 @@
+#ifndef TSAMPLEINFO_CXX
+#define TSAMPLEINFO_CXX
+#include "TSampleInfo.h"
+#include "TCollection.h"
+#include <algorithm>
+ClassImp(TSampleInfo)
+TSampleInfo::TSampleInfo() {}
+TSampleInfo::~TSampleInfo() {}
+
+TSampleInfo::TSampleInfo(double E,const char* name,const char* type,const char* pr,const char* files,int ev,int rb,int re,
+                         double sig,double lum ,double w)
+{
+    //fName=name;
+    SetName(name);
+    fE=E;
+    fPeriod=pr;
+    fType=type;
+    fFiles=files;
+    fEvents=ev;
+    fRunsBegin=rb;
+    fRunsEnd=re;
+    fSigma=sig;
+    fLuminocity=lum;
+    fWeight=w;
+    char a[20];
+
+    sprintf(a,"%i",(int)(fE + 0.5));
+
+    fEnergyString=std::string(a);
+
+
+}
+void TSampleInfo::Print()
+{
+
+    printf("%f %s %s %s  %i %i %i %f %f %f\n",
+
+           fE,
+           //  std::string fName;
+           fPeriod.c_str(),
+           fType.c_str(),
+           fFiles.c_str(),
+           fEvents,
+           fRunsBegin,
+           fRunsEnd,
+           fSigma,
+           fLuminocity,
+           fWeight);
+
+}
+#endif
+
---
+++ src/TSampleInfo.h
@@ -0,0 +1,28 @@
+#ifndef TSAMPLEINFO_H
+#define TSAMPLEINFO_H
+#include "TObject.h"
+#include "TNamed.h"
+class TSampleInfo: public TNamed
+{
+public:
+    TSampleInfo();// : TGraphAsymmErrors(n){};
+    //TAdvancedGraph(Int_t n, const Double_t* x, const Double_t* y, const Double_t* exl = 0, const Double_t* exh = 0, const Double_t* eyl = 0, const Double_t* eyh = 0);
+    TSampleInfo(double,const char*,const char*,const char*,const char*,int,int,int,double,double,double);
+    void Print();
+    double fE;
+    //  std::string fName;
+    std::string      fPeriod;
+    std::string      fType;
+    std::string      fFiles;
+    int          fEvents;
+    int          fRunsBegin;
+    int          fRunsEnd;
+    double       fSigma;
+    double       fLuminocity;
+    double fWeight;
+    std::string fEnergyString;
+    ~TSampleInfo();
+    ClassDef(TSampleInfo,1)
+
+};
+#endif
