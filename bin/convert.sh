9bb5d3ae33d6c47c90390a195c5122e4c27dce68
andriish <andriish@users.noreply.github.com>
2015-09-15 17:45:48 +0200

    Cleanup

--- bin/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-export LD_LIBRARY_PATH=$(pwd)/YODA-1.0.6/z/lib:$LD_LIBRARY_PATH
-		for a in $(find ee_antikt | grep rzhist ); do 
-		b=$(echo $a | sed 's@.rzhist@@g');
-		h2root $b".rzhist";    
-		./ReaderROOTtest $b".root"  $b".yoda";    
-		done
