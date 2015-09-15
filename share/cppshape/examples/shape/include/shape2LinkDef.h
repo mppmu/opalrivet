474a57b0d3491a2ef58917e30a087206e7c3558a
andriish <andriish@users.noreply.github.com>
2015-09-15 18:09:10 +0200

    OK

--- share/cppshape/examples/shape/include/shape2LinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class ntuple_107j;
-#pragma link C++ class ntuple_108;
-#pragma link C++ class ntuple_200;
-#pragma link C++ class ntuple_200j;
-#pragma link C++ class ntuple_200_lep1;
-#endif
-
--- share/cppshape/include/shapePAWLinkDef.h
+++
@@ -1,7 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class S4sub;
-#pragma link C++ class MomHist;
-#pragma link C++ class Hismanip;
-#pragma link C++ class Histogram;
-#endif
-
