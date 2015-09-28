9153786c03751b7afd0add3f4bb4b85a9535e207
andriish <andriish@users.noreply.github.com>
2015-09-28 18:28:29 +0200

    OK

--- bin/manyplots.sh
+++ bin/manyplots.sh
@@ -4,6 +4,13 @@ export number=$1
 export string=$(echo $number | sed 's@\.@dot@g' | sed 's@1@one@g' | sed 's@2@two@g' | sed 's@3@three@g' | sed 's@4@four@g' | sed 's@5@five@g' | sed 's@6@six@g'  | sed 's@7@seven@g'  | sed 's@8@eight@g' | sed 's@9@nine@g'  | sed 's@0@zero@g' )
 
 
+echo '
+%loop over all algorithms
+\FloatBarrier
+\subsection{Results for $\sqrt{s}='$number'\GeV$}
+\FloatBarrier'
+
+
 for algorithm in $(echo $algorithms | sed 's@:@ @g'); do
 echo '
 %loop over all algorithms
